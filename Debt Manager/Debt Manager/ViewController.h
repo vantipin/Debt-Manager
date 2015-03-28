113315291f1973fb60a94ec8378af16f751c2176
Pavel Stoma <jerkupp@gmail.com>
2015-03-28 08:53:56 +0300

    Initial commit

---
+++ Debt Manager/Debt Manager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Debt Manager
+//
+//  Created by Pavel Stoma on 3/28/15.
+//  Copyright (c) 2015 DM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
