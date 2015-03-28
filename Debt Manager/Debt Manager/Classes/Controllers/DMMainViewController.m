017fccb59c6ad39feb841f96935f228799de0433
Pavel Stoma <jerkupp@gmail.com>
2015-03-28 16:55:04 +0300

    [*] - get contacts configured

--- Debt Manager/Debt Manager/Classes/Controllers/DMMainViewController.m
+++ Debt Manager/Debt Manager/Classes/Controllers/DMMainViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "DMMainViewController.h"
+#import "APAddressBook.h"
+#import "APContact.h"
 
 @interface DMMainViewController ()
 
@@ -16,6 +18,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    APAddressBook *addressBook = [[APAddressBook alloc] init];
+    addressBook.sortDescriptors = @[
+                                    [NSSortDescriptor sortDescriptorWithKey:@"firstName" ascending:YES],
+                                    [NSSortDescriptor sortDescriptorWithKey:@"lastName" ascending:YES]
+                                    ];
+    
+    addressBook.fieldsMask = APContactFieldFirstName | APContactFieldLastName | APContactFieldPhones | APContactFieldEmails | APContactFieldThumbnail;
+    
+    [addressBook loadContacts:^(NSArray *contacts, NSError *error) {
+        for (APContact *contact in contacts) {
+            NSLog(@"%@ %@.\n%@\n%@", contact.firstName, contact.lastName, contact.phones, contact.emails);
+        }
+    }];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
